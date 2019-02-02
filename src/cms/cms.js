f24ada257f4d2c1f4b3bed1ddcf22b6c8a530f97
Adam Sanderson <Adam.Sanderson@theaccessgroup.com>
2019-02-02 20:36:11 +0000

    test process env

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -8,6 +8,8 @@ CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("products", ProductPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
 
+console.log('PROCESS ENV ::::::::', process.env);
+
 init({
   config: {
     media_library: {
